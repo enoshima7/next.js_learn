8733311cbe4bee5b543c2ae8e4eb1684bc6a37bd
enoshima7 <duanjipeng0328@gmail.com>
2020-04-14 12:35:42 +0800

    路由钩子函数

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,31 @@ import Link from 'next/link'
 import Router from 'next/router'
 
 const Home = () => {
+  // routeChangeStart
+  // routeChangeComplete
+  // beforeHistoryChange
+  // routeChangeError
+  // hashChangeStart
+  // hashChangeComplete
+  Router.events.on('routeChangeStart', (...args) => {
+    console.log('1. routeChangeStart=>路由开始变化，参数为', ...args)
+  })
+  Router.events.on('beforeHistoryChange', (...args) => {
+    console.log('2. beforeHistoryChange=>路由变化之前，参数为', ...args)
+  })
+  Router.events.on('routeChangeComplete', (...args) => {
+    console.log('3. routeChangeComplete=>路由变化结束，参数为', ...args)
+  })
+  Router.events.on('routeChangeError', (...args) => {
+    console.log('4. routeChangeError=>路由发生错误，参数为', ...args)
+  })
+  Router.events.on('hashChangeStart', (...args) => {
+    console.log('5. hashChangeStart=>hash路由变换之后，参数为', ...args)
+  })
+  Router.events.on('hashChangeComplete', (...args) => {
+    console.log('6. hashChangeComplete=>hash路由变化之前，参数为', ...args)
+  })
+
   function gotoXJJ() {
     Router.push({
       pathname: '/xjj',
@@ -19,6 +44,9 @@ const Home = () => {
         <br />
         <Link href='/xjj?name=cjk'>
           <a>选择仓进空</a>
+        </Link>
+        <Link href='#jspang'>
+          <a>选择技术胖</a>
         </Link>
       </div>
       <div>
