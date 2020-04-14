5715211bc39c6c6d9c656cbd20ffe4a034369305
enoshima7 <duanjipeng0328@gmail.com>
2020-04-14 13:14:30 +0800

    使用style jsx改变央视

---
+++ pages/jspang.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+function jspang() {
+  const [color, setColor] = useState('blue')
+  const changeColor = () => {
+    setColor(color == 'blue' ? 'red' : 'blue')
+  }
+  return (
+    <>
+      <div>技术胖免费前端教程</div>
+      <div className='jspang'>技术胖免费前端教程</div>
+      <div>
+        <button onClick={changeColor}>改变颜色</button>
+      </div>
+      <style jsx>
+        {`
+          div {
+            color: ${color};
+          }
+        `}
+      </style>
+    </>
+  )
+}
+export default jspang
