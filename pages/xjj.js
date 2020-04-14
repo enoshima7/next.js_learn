ee5a4e603253105abcd783ccd43cc62cba300ccb
enoshima7 <duanjipeng0328@gmail.com>
2020-04-14 13:02:53 +0800

    axios远程获取数据

--- pages/xjj.js
+++ pages/xjj.js
@@ -1,8 +1,11 @@
 import { withRouter } from 'next/router'
 import Link from 'next/link'
-const xjj = ({ router }) => {
+import axios from 'axios'
+
+const xjj = ({ router, data }) => {
   return (
     <>
+      <div>{data}</div>
       <div>{router.query.name} 来服务了</div>
       <Link href='/'>
         <a>返回首页</a>
@@ -10,5 +13,13 @@ const xjj = ({ router }) => {
     </>
   )
 }
-
+xjj.getInitialProps = async () => {
+  const promise = new Promise((resolve) => {
+    axios('http://localhost:4001/data/data.json').then((res) => {
+      console.log('远程数据', res.data.data)
+      resolve(res.data)
+    })
+  })
+  return await promise
+}
 export default withRouter(xjj)
