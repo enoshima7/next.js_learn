3b06e912bde0db510bd3a2077ebd7b25ada74b24
enoshima7 <duanjipeng0328@gmail.com>
2020-04-14 14:13:09 +0800

    按需引入antd和babel配置以及css用import引入

--- pages/Header.js
+++ pages/Header.js
@@ -1,9 +1,14 @@
 import MyHead from '../components/Head'
+import '../static/test.css'
+import { Button } from 'antd'
 function header() {
   return (
     <>
       <MyHead />
       <div>Head</div>
+      <div>
+        <Button>点击</Button>
+      </div>
     </>
   )
 }
